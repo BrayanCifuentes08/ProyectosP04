a9ca267e9fc5acdb8dfee0eebf5bff0a010c4592
BrayanCifuentes08 <developer04@demosoftonline.com>
2024-11-08 14:59:51 -0600

    subiendo api actualizada con controlador de crud User

---
+++ MTTO/ApiMantenimientos/ApiMantenimientos/Models/PaCrudTipoCanalDistribucionM.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiMantenimientos.Models
+{
+    public class PaCrudTipoCanalDistribucionM
+    {
+        public int? TipoCanalDistribucion { get; set; }
+        public string? Descripcion { get; set; }
+        public int? Estado { get; set; }
+        public DateTime? Fecha_Hora { get; set; }
+        public string? UserName { get; set; }
+        public string? Mensaje { get; set; }
+        public bool Resultado { get; set; }
+    }
+}
