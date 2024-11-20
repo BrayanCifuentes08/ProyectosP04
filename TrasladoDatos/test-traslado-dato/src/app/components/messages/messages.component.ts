e0aef6e6351930deb9830a0aa86c70ac46d5afe9
JuanJoseMayen <developer05@demosoftonline.com>
2024-11-20 13:13:30 -0600

    Cambios despues del error de index.lock

--- TrasladoDatos/test-traslado-dato/src/app/components/messages/messages.component.ts
+++ TrasladoDatos/test-traslado-dato/src/app/components/messages/messages.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent {
+  @Input() mensajeAlerta: string = ''; // Mensaje para la alerta
+  @Input() mensajeModal: string = '';  // Mensaje para el modal
+  @Input() mensajeExito: string = '';
+  @Input() isVisibleAlerta: boolean = false; // Controla la visibilidad de la alerta
+  @Input() isVisibleModal: boolean = false;  // Controla la visibilidad del modal
+  @Input() isVisibleExito: boolean = false;
 
+  @Output() confirmarAccion = new EventEmitter<void>();
+  @Output() cancelarAccion = new EventEmitter<void>();
+  @Output() cerrar = new EventEmitter<void>(); // Para cerrar la alerta
+
+  constructor() {}
+
+  ocultarAlerta() {
+    this.cerrar.emit(); 
+  }
+
+  ocultarModal() {
+    this.isVisibleModal = false;
+  }
+
+  confirmar() {
+    this.confirmarAccion.emit();
+    this.ocultarModal();
+  }
+
+  cancelar() {
+    this.cancelarAccion.emit();
+    this.ocultarModal();
+  }
+  ocultarExito() {
+    this.isVisibleExito = false;
+  }
 }
