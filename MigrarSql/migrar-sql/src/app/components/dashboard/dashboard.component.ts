040eb8b210f43edf1f23a8a46f73e57c853097bf
BrayanCifuentes08 <developer04@demosoftonline.com>
2024-12-05 12:21:47 -0600

    mejorando interfaz y trabajando en captura de ruta de archivo

--- MigrarSql/migrar-sql/src/app/components/dashboard/dashboard.component.ts
+++ MigrarSql/migrar-sql/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { MigrarSqlService } from '../../services/migrar-sql.service';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
@@ -13,25 +16,53 @@ export default class DashboardComponent {
   hojas: string[] = [];
   hojaSeleccionada: string = '';
   cargandoHojas: boolean = false;
-  cargandoTraslado: boolean = false;
   mostrarMensajeAdvertencia: boolean = false;
   isVisibleModal: boolean = false;
   isVisibleExito: boolean = false;
   mensajeExito: string = '';
   isVisibleAlerta: boolean = false;
   mensajeAlerta: string = '';
-  usuario: string = '';
-  horaInicioSesionFormatted: string | null = null;
-  fechaVencimientoToken: string | null = null;
-  usandoHoraPerma: boolean = false;
-  tooltipVisible: boolean = false;
-  tiempoRestante: string = '';
   @ViewChild('fileInput') fileInput: ElementRef | undefined;
-
-  constructor(private sharedService:SharedService){}
+  rutaOrigen: string | null = null;
+  mostrarArea: boolean = false;
+  constructor(private sharedService:SharedService, private migrarSqlService: MigrarSqlService){}
 
   ngOnInit(){
     this.sharedService.setAccion('modulo');
+  }
+
+  cargarFile(event: any): void {
+    const file: File = event.target.files[0];
+    // Validar si ya hay un archivo cargado
+    if (this.fileSeleccionado) {
+      this.mostrarMensajeAdvertencia = true;
+      event.target.value = '';  // Limpiar el input de archivo
+      setTimeout(() => {
+        this.mostrarMensajeAdvertencia = false;
+      }, 7000);
+      return;
+    }
+    this.mostrarArea = false;
+    this.hojas = [];
+    if (file) {
+      this.fileSeleccionado = file;
+
+      console.log("Hojas antes de solicitud: ", this.hojas)
+      console.log('Archivo seleccionado:', file);
+      this.cargandoHojas = true;
+      // Llamada al método obtenerHojasExcel
+      this.migrarSqlService.obtenerHojasExcel(file).subscribe({
+        next: (hojas: string[]) => {
+          this.hojas = hojas; // Asigna las hojas obtenidas a la variable local
+          this.cargandoHojas = false;
+          console.log('Hojas obtenidas:', hojas);
+        },
+        error: (err) => {
+          console.error('Error al obtener hojas:', err); // Manejo de errores
+          this.cargandoHojas = false;
+        }
+      });
+    }
   }
 
   //Función para borrar el archivo
